964cdc97299bcf4f73326ed24e4f9402cb11abc6
Ellona Macmillan <macmi2el@dukes.jmu.edu>
2025-08-11 14:48:56 -0400

    starting english testing

--- test/english.js
+++ test/english.js
@@ -15,3 +15,40 @@ function testTranslation(sample, ast) {
   });
   it(`should translate from praxis to english`, () => assert.equal(targetSource, sample.english));
 }
+
+function testProgram(sample) {
+  describe(`// ${sample.message}\n${sample.source}`, () => {
+    const tokens = praxis.lex(sample.source);
+    const ast = praxis.parse(tokens, sample.source);
+
+    if (sample.translation) {
+      testTranslation(sample, ast);
+    }
+
+    it(`should output\n${sample.output}`, async () => {
+      const logger = makeLogger();
+      const runtime = new GlobalRuntime(logger.log, getInput, false, 'this');
+      await ast.visit(new Evaluator(new praxis.OutputFormatter(), new Memdia()), runtime);
+      assert.equal(logger.stdout, sample.output);
+    });
+  });
+}
+
+describe ("English : Translate Programs to from Praxis to english", () => {
+  const samples = [
+    {
+      message: "Class Dog",
+      praxis:
+      `class Dog
+        String name = "Bella"
+        void bark()
+          print "woof"
+        end bark
+      end class Dog`,
+      english:
+      `Define a class named Dog. It includes 1 instance variable, a variable named "name" of type String with the value "Bella" and a method called bark that takes no parameters. When called, the method will print "woof".`
+    }
+  ];
+
+  samples.forEach(testProgram);
+});
