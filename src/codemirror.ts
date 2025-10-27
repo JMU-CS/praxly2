6a013a411fbc0f7da561dc3e1aba1d7b9b975658
Chris Johnson <crjjrc@gmail.com>
2025-10-27 17:26:05 -0400

    Add tentative syntax highlight for Java and Python

--- src/codemirror.ts
+++ src/codemirror.ts
@@ -1,10 +1,9 @@
 export {lineNumbers, highlightActiveLineGutter, highlightSpecialChars, drawSelection, dropCursor, rectangularSelection, crosshairCursor, highlightActiveLine, keymap} from '@codemirror/view';
-export {foldGutter, indentOnInput, syntaxHighlighting, defaultHighlightStyle, bracketMatching, foldKeymap} from '@codemirror/language';
+export {foldGutter, indentOnInput, syntaxHighlighting, defaultHighlightStyle, bracketMatching, foldKeymap, LanguageSupport} from '@codemirror/language';
 export {history, defaultKeymap, historyKeymap, indentWithTab} from '@codemirror/commands';
 export {searchKeymap} from '@codemirror/search';
 export {closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap} from '@codemirror/autocomplete';
 export {lintKeymap} from '@codemirror/lint';
-export {EditorState, EditorSelection} from '@codemirror/state';
-export {StateField, StateEffect, Transaction, Range} from '@codemirror/state';
+export {EditorState, EditorSelection, Compartment, StateField, StateEffect, Transaction, Range} from '@codemirror/state';
 export {EditorView, Decoration} from '@codemirror/view';
 export {toggleComment} from '@codemirror/commands';
